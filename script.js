9bce0d4e61f289a5b3fa49f6fd656c75dc1e9417
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-11-02 13:06:11 -0300

    2 little fine number conversion added

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const btnCal = document.querySelector('#btnCal');
 const pResult = document.querySelector('#result')
 
 function onClickBtn(){
-	let total = parseInt(input1.value) + parseInt(input2.value);
+	let total = Number(input1.value) + parseInt(input2.value);
+    
+    // there's even a simpler way to transform to numbers, and that is by placing a + before the object call
+    console.log(+input1.value + +input2.value);
+
     pResult.innerHTML= "The result is: " + total;
     pResult.classList.add('show-p');
     pResult.classList.remove('hide-p');
